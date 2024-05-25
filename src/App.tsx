73f57faca1187647da6443b913c25161f9986933
TRACKFARM_MICHI\tfmic <michioka@trackfarm.co.jp>
2024-05-25 23:05:46 +0900

    ダミーデータ表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import { dummyTodoList } from './data/dummyTodoList';
 
 function App() {
   return (
-    <main className="mx-auto mt-10 max-w-xl">
-      <h1 className="text-center text-4xl">Todoアプリ</h1>
+    <main className="mx-auto mt-10 max-w-xl space-y-10">
+      <h1 className=" text-4xl text-center">Todoアプリ</h1>
+      <div className="rounded bg-slate-200 p-5">
+        <div className="space-y-3">
+          {dummyTodoList.map((todo) => (
+            <p key={todo.id}>{todo.title}</p>
+          ))}
+        </div>
+      </div>
     </main>
-  )
+  );
 }
 
 export default App;
---
+++ src/data/dummyTodoList.ts
@@ -0,0 +1,19 @@
+import { Todo } from '../assets/todo';
+
+export const dummyTodoList: Todo[] = [
+  {
+    id: 1,
+    title: 'Reactをべんきょうする',
+    completed: false,
+  },
+  {
+    id: 2,
+    title: '夕食を作る',
+    completed: false,
+  },
+  {
+    id: 3,
+    title: '部屋を掃除する',
+    completed: true,
+  },
+];
