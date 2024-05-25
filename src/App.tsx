095dabcaa187b5399c2b625d77c1d73ccf9f00f8
TRACKFARM_MICHI\tfmic <michioka@trackfarm.co.jp>
2024-05-26 02:41:42 +0900

    タスク削除機能追加

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { dummyTodoList } from './data/dummyTodoList';
 import { TodoList } from './components/TodoList';
 import { useState } from 'react';
 import { AddTodoForm } from './components/AddTodoForm';
+import { TodoSummary } from './components/TodoSummary';
 
 function App() {
   const [todoList, setTodoList] = useState(dummyTodoList);
@@ -41,16 +42,25 @@ function App() {
     });
   };
 
+  const deleteAllCompleted = () => {
+    setTodoList((prevTodoList) => {
+      return prevTodoList.filter((todo) => {
+        return !todo.completed;
+      });
+    });
+  };
+
   return (
     <main className="mx-auto mt-10 max-w-xl space-y-10">
       <h1 className=" text-4xl text-center">Todoアプリ</h1>
       <AddTodoForm addTodo={addTodo} />
-      <div className="rounded bg-slate-200 p-5">
+      <div className="space-y-5 rounded bg-slate-200 p-5">
         <TodoList
           todoList={todoList}
           changeCompleted={changeCompleted}
           deleteTodo={deleteTodo}
         />
+        <TodoSummary deleteAllCompleted={deleteAllCompleted} />
       </div>
     </main>
   );
