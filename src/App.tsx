31dd3e9c9a522a69f9336712ce3a1e06935031ab
TRACKFARM_MICHI\tfmic <michioka@trackfarm.co.jp>
2024-05-25 23:13:45 +0900

    コンポーネント分割

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import { dummyTodoList } from './data/dummyTodoList';
+import { TodoList } from './components/TodoList';
 
 function App() {
   return (
     <main className="mx-auto mt-10 max-w-xl space-y-10">
       <h1 className=" text-4xl text-center">Todoアプリ</h1>
       <div className="rounded bg-slate-200 p-5">
-        <div className="space-y-3">
-          {dummyTodoList.map((todo) => (
-            <p key={todo.id}>{todo.title}</p>
-          ))}
-        </div>
+        <TodoList todoList={dummyTodoList} />
       </div>
     </main>
   );
